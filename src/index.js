4af18fa057ac8b1da2e77a23eae663de94e31f03
Tania Holst <holst.tania@gmail.com>
2018-10-21 21:22:39 +0200

    #9 added images for background and index.css

--- src/index.js
+++ src/index.js
@@ -2,19 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 //import Counter from "./components/counter";
+import "./index.css";
+
 import NavBar from "./components/navbar";
-/*import './index.css';
+
+/*
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
- */
 
 /* const element = <h1>Hello World</h1>;
 
@@ -24,4 +22,4 @@ ReactDOM.render(element, document.getElementById("root")); */
 ReactDOM.render(<NavBar />, document.getElementById("root"));
 
 
-// https://www.youtube.com/watch?v=Ke90Tje7VS0          21.38
+// https://www.youtube.com/watch?v=Ke90Tje7VS0
