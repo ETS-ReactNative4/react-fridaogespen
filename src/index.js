cdc8f79309e794bfe9fdc65606d0c5e62ae1ed82
Tania Holst <holst.tania@gmail.com>
2018-10-21 20:20:54 +0200

    #7 started on navbar, added react-bootstrap to dependency

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+//import Counter from "./components/counter";
+import NavBar from "./components/navbar";
+/*import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +14,14 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+ */
+
+/* const element = <h1>Hello World</h1>;
+
+ReactDOM.render(element, document.getElementById("root")); */
+
+//ReactDOM.render(<Counter />, document.getElementById("root"));
+ReactDOM.render(<NavBar />, document.getElementById("root"));
+
+
+// https://www.youtube.com/watch?v=Ke90Tje7VS0          21.38
