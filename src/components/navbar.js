7e3c11bcdca8698875fc36eea1ab57283fae1004
Tania Holst <holst.tania@gmail.com>
2018-11-10 15:00:43 +0100

    #7, #9, fixed responsive navbar and background image

---
+++ src/components/navbar.js
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarNav,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  NavLink
+} from "mdbreact";
+import { BrowserRouter as Router } from "react-router-dom";
+import "../stylesheet/App.css";
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+      isWideEnough: false,
+      pageImages: false,
+      pageWeddingday: false,
+      pageWishlist: false,
+      pageContacts: false,
+      pageRSVP: false
+    };
+    this.collapseOnClick = this.collapseOnClick.bind(this);
+  }
+
+  collapseOnClick() {
+    this.setState({
+      collapse: !this.state.collapse
+    });
+  }
+
+  showPage() {
+    
+  }
+
+  render() {
+    return (
+      <Router>
+        <Navbar light expand="md" scrolling id="navbarContainer">
+          <NavbarBrand id="navbarHeader">
+            <strong>Frida&Espen</strong>
+          </NavbarBrand>
+          {!this.state.isWideEnough && <NavbarToggler onClick={this.collapseOnClick} />}
+          <Collapse isOpen={this.state.collapse} navbar>
+            <NavbarNav center id="navbarItemlist">
+              <NavItem active>
+                <NavLink className="navLink" value="frontPage" to="#">Forside</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="navLink" to="#">Bilder</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="navLink" to="#">Bryllupsside</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="navLink" to="#">Ønskeliste</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="navLink" to="#">Kontakter</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="navLink" to="#">RSVP</NavLink>
+              </NavItem>
+            </NavbarNav>
+          </Collapse>
+        </Navbar>
+      </Router>
+    );
+  }
+}
+
+export default NavBar;
