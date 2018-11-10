ab9e7bed9c7d17cf9c75d651e3ce0a220a9ca105
Tania Holst <holst.tania@gmail.com>
2018-11-10 19:06:54 +0100

    Revert "Feat 10 functionality tabs"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, { Component } from "react";
-
-import "bootstrap/dist/css/bootstrap.css";
-import "./stylesheet/App.css";
-import NavBar from "./components/navbar.js";
-import Pages from "./pages/pages.js";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="appHeader">
-          <NavBar />
-        </div>
-        <div className="pageContainer">
-          <Pages />
-        </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
