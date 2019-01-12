835a166c1365a7f38714ed3f94c72d9df9ffe3d8
Tania Holst <holst.tania@gmail.com>
2019-01-12 22:39:08 +0100

    #24 open cleanup-branch and added smaller changes

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
-//import Counter from "./components/counter";
-import NavBar from "./components/navbar";
-/*import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from "./App";
+import './stylesheet/App.css';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
- */
-
-/* const element = <h1>Hello World</h1>;
-
-ReactDOM.render(element, document.getElementById("root")); */
-
-//ReactDOM.render(<Counter />, document.getElementById("root"));
-ReactDOM.render(<NavBar />, document.getElementById("root"));
-
-
-// https://www.youtube.com/watch?v=Ke90Tje7VS0          21.38
+ReactDOM.render(<App />, document.getElementById("root"));
