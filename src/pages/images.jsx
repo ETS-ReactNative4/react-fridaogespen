32a272d58b0240633c0475022f845e4e4ca6f7bd
Tania Holst <holst.tania@gmail.com>
2019-01-16 21:41:10 +0100

    #2 image carousel fixed and images added

--- src/pages/images.jsx
+++ src/pages/images.jsx
@@ -5,63 +5,67 @@ import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
 export default class Images extends Component {
   render() {
     return (
-      <div className="pageContainer">
-        <h1>Bilder</h1>
-
-        <Carousel
-          autoPlay
-          transitionTime="1000"
-          interval="5000"
-          showThumbs="false"
-          infiniteLoop="true"
-          stopOnHover="true"
-          dynamicHeight="true"
-        >
-          <div>
-            <img alt="" src={require("../images/imageCarousel/IMG_0849.jpg")} />
-          </div>
-          <div>
-            <img alt="" src={require("../images/imageCarousel/IMG_0851.jpg")} />
-          </div>
-          <div>
-            <img alt="" src={require("../images/imageCarousel/IMG_1657.jpg")} />
-          </div>
-          <div>
-            <img alt="" src={require("../images/imageCarousel/IMG_1786.jpg")} />
-          </div>
-          <div>
-            <img alt="" src={require("../images/imageCarousel/IMG_2047.jpg")} />
-          </div>
-          <div>
-            <img alt="" src={require("../images/imageCarousel/IMG_2048.jpg")} />
-          </div>
-          <div>
-            <img alt="" src={require("../images/imageCarousel/IMG_2226.jpg")} />
-          </div>
-          <div>
-            <img alt="" src={require("../images/imageCarousel/IMG_2363.jpg")} />
-          </div>
-          <div>
-            <img alt="" src={require("../images/imageCarousel/IMG_2452.jpg")} />
-          </div>
-          <div>
-            <img alt="" src={require("../images/imageCarousel/IMG_3004.jpg")} />
-          </div>
-          <div>
-            <img alt="" src={require("../images/imageCarousel/IMG_3041.jpg")} />
-          </div>
-          <div>
-            <img alt="" src={require("../images/imageCarousel/IMG_3499.jpg")} />
-          </div>
-          <div>
-            <img alt="" src={require("../images/imageCarousel/IMG_4153.jpg")} />
-          </div>
-          <div>
-            <img alt="" src={require("../images/imageCarousel/IMG_4269.jpg")} />
-          </div>
-        </Carousel>
+      <div className="pageContainer imageContainer">
+        <h1 id="imageHeader">Bilder</h1>
+        <div id="imageCarousel">
+          <Carousel
+            autoPlay
+            transitionTime="1000"
+            interval="5000"
+            showThumbs="false"
+            infiniteLoop="true"
+            stopOnHover="true"
+            dynamicHeight="true"
+            showIndicators="false"
+          >
+            <div>
+              <img alt="" src={require("../images/imageCarousel/img1.JPG")} />
+            </div>
+            <div>
+              <img alt="" src={require("../images/imageCarousel/img2.JPG")} />
+            </div>
+            <div>
+              <img alt="" src={require("../images/imageCarousel/img3.JPG")} />
+            </div>
+            <div>
+              <img alt="" src={require("../images/imageCarousel/img4.JPG")} />
+            </div>
+            <div>
+              <img alt="" src={require("../images/imageCarousel/img5.JPG")} />
+            </div>
+            <div>
+              <img alt="" src={require("../images/imageCarousel/img6.JPG")} />
+            </div>
+            <div>
+              <img alt="" src={require("../images/imageCarousel/img7.JPG")} />
+            </div>
+            <div>
+              <img alt="" src={require("../images/imageCarousel/img8.JPG")} />
+            </div>
+            <div>
+              <img alt="" src={require("../images/imageCarousel/img9.JPG")} />
+            </div>
+            <div>
+              <img alt="" src={require("../images/imageCarousel/img10.JPG")} />
+            </div>
+            <div>
+              <img alt="" src={require("../images/imageCarousel/img11.JPG")} />
+            </div>
+            <div>
+              <img alt="" src={require("../images/imageCarousel/img12.JPG")} />
+            </div>
+            <div>
+              <img alt="" src={require("../images/imageCarousel/img13.JPG")} />
+            </div>
+            <div>
+              <img alt="" src={require("../images/imageCarousel/img14.JPG")} />
+            </div>
+            <div>
+              <img alt="" src={require("../images/imageCarousel/img15.JPG")} />
+            </div>
+          </Carousel>
+        </div>
       </div>
     );
   }
 }
-
