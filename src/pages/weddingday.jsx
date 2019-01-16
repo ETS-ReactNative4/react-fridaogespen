941ca8b3605a197b14fad9dccae2cbe7b05e8a59
Tania Holst <holst.tania@gmail.com>
2019-01-16 23:44:05 +0100

    #3 #4 #5 added information, waiting for last images in contacts

--- src/pages/weddingday.jsx
+++ src/pages/weddingday.jsx
@@ -5,35 +5,61 @@ class Weddingday extends Component {
   render() {
     return (
       <div className="pageContainer">
-        <h1>Bryllupsdagen</h1>
+        <h1>Hotell</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Bryllupshelgen foregår på{" "}
+          <a
+            href="https://www.klaekken.no/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Klækken Hotell
+          </a>{" "}
+          i Hønefoss. Alle gjester som ønsker å bo der kan kontakte Klækken for
+          å booke rom ved å ringe 321 40 000. For rabatt kan dere oppgi at dere
+          skal i vårt bryllup.
         </p>
-        <h1>Vielsen</h1>
+
+        <h1>Barn</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Vi ordner lekerom for de minste, og de kan gjerne ta med en
+          favorittleke eller to. Alle barn som vil ha egen barnemeny må kontakte
+          oss, hvis ikke får de en barneversjon av den egentlige menyen. Ta også
+          kontakt hvis noen barn/babyer trenger barnestol.
         </p>
-        <h1>Festen</h1>
+
+        <h1>Ankomst</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Askeladden Reiser kjører buss fra Gardermoen til Klækken Hotell.
+          <br />{" "}
+          <a
+            href="https://askeladden.net/flybuss/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Rutetabellen kan leses her
+          </a>
+        </p>
+
+        <h1>Fredag 02. August</h1>
+        <p>
+          Kl 19.00 arrangeres det grillkveld på Klækken Hotell med quiz etter
+          middagen. Middagen er valgfri og koster 295,- per person. Hvis det er
+          fint vær sitter vi ute, og Klækken stiller da opp med bar ute for oss.
+          <br />
+          Dersom du ikke ønsker å spise middag kan du allikevel være med på
+          quizen. Husk da å huk av for underholdning i RSVP.
+        </p>
+
+        <h1>Lørdag 03. August</h1>
+        <p>
+          Kl 13.00 gifter vi oss i Norderhov Kirke. Bryllupsmottakelsen vil være
+          på Klækken Hotell ca kl 16.00, og vi går til bords rundt kl 16.30 hvor
+          det blir servert treretters meny.
+          <br />
+          Alle med allergier må si ifra ved å skrive det under "Alleriger" i
+          RSVP, eller kontakte oss direkte. Kaffegjestene er invitert til kl
+          20.30, men må gjerne komme i vielsen også.
         </p>
       </div>
     );
--- src/pages/wishlist.jsx
+++ src/pages/wishlist.jsx
@@ -4,9 +4,23 @@ class Wishlist extends Component {
   state = {};
   render() {
     return (
-        <div className="pageContainer">
+      <div className="pageContainer">
         <h1>Ønskeliste</h1>
-        
+        <a
+          href="https://www.tilbords.no/bryllup/vis-liste/?wishListId=46822"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tilbords
+        </a>
+        <br />
+        <a
+          href="https://www.listfully.no/share/npQ1Aeqssta7sJSoIgUh"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vår egen liste
+        </a>
       </div>
     );
   }
